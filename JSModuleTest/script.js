043b1f37f216dc2e41c9b960cd1e36f9f8580977
raaronshaw <rashaw@gmail.com>
2025-04-23 01:36:07 -0700

    testing imports and exports and js classes/objects

--- JSModuleTest/script.js
+++ JSModuleTest/script.js
@@ -1,8 +1,9 @@
 import { sum, difference, product, quotient } from './functions.js'
-
+import {vec22} from './gl-matrix-modified.js'
+//const Ls = vec3(1.0,1.0,1.0)
 const x = 10
 const y = 5
-
+const moo = vec22(1,2)
 document.getElementById('x').textContent = x
 document.getElementById('y').textContent = y
 
@@ -10,3 +11,5 @@ document.getElementById('addition').textContent = sum(x, y)
 document.getElementById('subtraction').textContent = difference(x, y)
 document.getElementById('multiplication').textContent = product(x, y)
 document.getElementById('division').textContent = quotient(x, y)
+//console.log(moo.create())
+document.getElementById('test import').textContent = moo
