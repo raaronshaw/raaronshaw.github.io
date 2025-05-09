29f52db418756e1a083d577fe428476fa633f62a
raaronshaw <rashaw@gmail.com>
2025-05-09 01:36:19 -0700

    refactored glsl

--- JSModuleTest/glslSrc.js
+++ JSModuleTest/glslSrc.js
@@ -13,14 +13,16 @@ export const my_glsl_vs_str = `
     varying vec3 position_eye, normal_eye; \
     varying mat4 out_Matrix_View; \
     varying highp vec3 vLighting;
+    float mydeterminant(mat4);
+    mat4 myidentity();
     mat4 myinverse(mat4);
     mat4 mytranspose(mat4);
     void main(void) { \
         out_Matrix_View = Matrix_View; \
         position_eye = vec3(Matrix_View * Matrix_Model * vec4(aVertexPosition,1.0)); \
         normal_eye = vec3(Matrix_View * Matrix_Model * vec4(aVertexNormals,0.0)); \
-        gl_PointSize = 2.0; \
-        gl_Position = Matrix_Projection * Matrix_View * Matrix_Model * vec4(aVertexPosition.x,aVertexPosition.y,aVertexPosition.z, 1.0); \
+    
+        gl_Position = Matrix_Projection * Matrix_View * Matrix_Model * vec4(aVertexPosition, 1.0); \
         out_Kd = Kd; \
         highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
         highp vec3 directionalLightColor = vec3(1, 1, 1);
@@ -32,129 +34,150 @@ export const my_glsl_vs_str = `
     
     mat4 myinverse(mat4 a)
     {
-        float a00 = a[0][0];
-        float a01 = a[0][1];
-        float a02 = a[0][2];
-        float a03 = a[0][3];
-        float a10 = a[1][0];
-        float a11 = a[1][1];
-        float a12 = a[1][2];
-        float a13 = a[1][3];
-        float a20 = a[2][0];
-        float a21 = a[2][1];
-        float a22 = a[2][2];
-        float a23 = a[2][3];
-        float a30 = a[3][0];
-        float a31 = a[3][1];
-        float a32 = a[3][2];
-        float a33 = a[3][3];
-        float b00 = a00 * a11 - a01 * a10;
-        float b01 = a00 * a12 - a02 * a10;
-        float b02 = a00 * a13 - a03 * a10;
-        float b03 = a01 * a12 - a02 * a11;
-        float b04 = a01 * a13 - a03 * a11;
-        float b05 = a02 * a13 - a03 * a12;
-        float b06 = a20 * a31 - a21 * a30;
-        float b07 = a20 * a32 - a22 * a30;
-        float b08 = a20 * a33 - a23 * a30;
-        float b09 = a21 * a32 - a22 * a31;
-        float b10 = a21 * a33 - a23 * a31;
-        float b11 = a22 * a33 - a23 * a32; 
+        float b00 = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+        float b01 = a[0][0] * a[1][2] - a[0][2] * a[1][0];
+        float b02 = a[0][0] * a[1][3] - a[0][3] * a[1][0];
+        float b03 = a[0][1] * a[1][2] - a[0][2] * a[1][1];
+        float b04 = a[0][1] * a[1][3] - a[0][3] * a[1][1];
+        float b05 = a[0][2] * a[1][3] - a[0][3] * a[1][2];
+        float b06 = a[2][0] * a[3][1] - a[2][1] * a[3][0];
+        float b07 = a[2][0] * a[3][2] - a[2][2] * a[3][0];
+        float b08 = a[2][0] * a[3][3] - a[2][3] * a[3][0];
+        float b09 = a[2][1] * a[3][2] - a[2][2] * a[3][1];
+        float b10 = a[2][1] * a[3][3] - a[2][3] * a[3][1];
+        float b11 = a[2][2] * a[3][3] - a[2][3] * a[3][2];
         float det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
-        //if (!det) {
-        //    return null;
-        //}
+        if (det==0.0) {
+            return myidentity();
+        }
         det = 1.0 / det;
-        mat4 result;
-        result[0][0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
-        result[0][1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
-        result[0][2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
-        result[0][3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
-        result[1][0] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
-        result[1][1] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
-        result[1][2] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
-        result[1][3] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
-        result[2][0] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
-        result[2][1] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
-        result[2][2] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
-        result[2][3] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
-        result[3][0] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
-        result[3][1] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
-        result[3][2] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
-        result[3][3] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
-        return result;
+        return mat4(
+        (a[1][1] * b11 - a[1][2] * b10 + a[1][3] * b09) * det,
+        (a[0][2] * b10 - a[0][1] * b11 - a[0][3] * b09) * det,
+        (a[3][1] * b05 - a[3][2] * b04 + a[3][3] * b03) * det,
+        (a[2][2] * b04 - a[2][1] * b05 - a[2][3] * b03) * det,
+        (a[1][2] * b08 - a[1][0] * b11 - a[1][3] * b07) * det,
+        (a[0][0] * b11 - a[0][2] * b08 + a[0][3] * b07) * det,
+        (a[3][2] * b02 - a[3][0] * b05 - a[3][3] * b01) * det,
+        (a[2][0] * b05 - a[2][2] * b02 + a[2][3] * b01) * det,
+        (a[1][0] * b10 - a[1][1] * b08 + a[1][3] * b06) * det,
+        (a[0][1] * b08 - a[0][0] * b10 - a[0][3] * b06) * det,
+        (a[3][0] * b04 - a[3][1] * b02 + a[3][3] * b00) * det,
+        (a[2][1] * b02 - a[2][0] * b04 - a[2][3] * b00) * det,
+        (a[1][1] * b07 - a[1][0] * b09 - a[1][2] * b06) * det,
+        (a[0][0] * b09 - a[0][1] * b07 + a[0][2] * b06) * det,
+        (a[3][1] * b01 - a[3][0] * b03 - a[3][2] * b00) * det,
+        (a[2][0] * b03 - a[2][1] * b01 + a[2][2] * b00) * det);
     }
     mat4 mytranspose(mat4 a)
     {
-        mat4 result;
-        result[0][0] = a[0][0];
-        result[0][1] = a[1][0];
-        result[0][2] = a[2][0];
-        result[0][3] = a[3][0];
-        result[1][0] = a[0][1];
-        result[1][1] = a[1][1];
-        result[1][2] = a[2][1];
-        result[1][3] = a[3][1];
-        result[2][0] = a[0][2];
-        result[2][1] = a[1][2];
-        result[2][2] = a[2][2];
-        result[2][3] = a[3][2];
-        result[3][0] = a[0][3];
-        result[3][1] = a[1][3];
-        result[3][2] = a[2][3];
-        result[3][3] = a[3][3];
-        return result;
+        return mat4(a[0][0], a[1][0], a[2][0], a[3][0], a[0][1], a[1][1], a[2][1], a[3][1], a[0][2], a[1][2], a[2][2], a[3][2], a[0][3], a[1][3], a[2][3], a[3][3]);
+    }
+    mat4 myidentity() { return mat4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0); }
+    float mydeterminant(mat4 a)
+    {
+        float b0 = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+        float b1 = a[0][0] * a[1][2] - a[0][2] * a[1][0];
+        float b2 = a[0][1] * a[1][2] - a[0][2] * a[1][1];
+        float b3 = a[2][0] * a[3][1] - a[2][1] * a[3][0];
+        float b4 = a[2][0] * a[3][2] - a[2][2] * a[3][0];
+        float b5 = a[2][1] * a[3][2] - a[2][2] * a[3][1];
+        float b6 = a[0][0] * b5 - a[0][1] * b4 + a[0][2] * b3;
+        float b7 = a[1][0] * b5 - a[1][1] * b4 + a[1][2] * b3;
+        float b8 = a[2][0] * b2 - a[2][1] * b1 + a[2][2] * b0;
+        float b9 = a[3][0] * b2 - a[3][1] * b1 + a[3][2] * b0;
+
+        // Calculate the determinant
+        return a[1][3] * b6 - a[0][3] * b7 + a[3][3] * b8 - a[2][3] * b9;
 
     }
 
 `;
 
+/*
+export function determinant(a) {
+  let a00 = a[0],
+    a01 = a[1],
+    a02 = a[2],
+    a03 = a[3];
+  let a10 = a[4],
+    a11 = a[5],
+    a12 = a[6],
+    a13 = a[7];
+  let a20 = a[8],
+    a21 = a[9],
+    a22 = a[10],
+    a23 = a[11];
+  let a30 = a[12],
+    a31 = a[13],
+    a32 = a[14],
+    a33 = a[15];
+
+  let b0 = a00 * a11 - a01 * a10;
+  let b1 = a00 * a12 - a02 * a10;
+  let b2 = a01 * a12 - a02 * a11;
+  let b3 = a20 * a31 - a21 * a30;
+  let b4 = a20 * a32 - a22 * a30;
+  let b5 = a21 * a32 - a22 * a31;
+  let b6 = a00 * b5 - a01 * b4 + a02 * b3;
+  let b7 = a10 * b5 - a11 * b4 + a12 * b3;
+  let b8 = a20 * b2 - a21 * b1 + a22 * b0;
+  let b9 = a30 * b2 - a31 * b1 + a32 * b0;
+
+  // Calculate the determinant
+  return a13 * b6 - a03 * b7 + a33 * b8 - a23 * b9;
+}
+
+*/
         
 
     export const my_glsl_vsSource = `
-      attribute vec4 aVertexPosition;
-      attribute vec3 aVertexNormal;
-      attribute vec2 aTextureCoord;
-    
-      uniform mat4 uNormalMatrix;
-      uniform mat4 uModelViewMatrix;
-      uniform mat4 uProjectionMatrix;
-    
-      varying highp vec2 vTextureCoord;
-      varying highp vec3 vLighting;
-    
-      void main(void) {
+    #version 100
+    attribute vec4 aVertexPosition;
+    attribute vec3 aVertexNormal;
+    attribute vec2 aTextureCoord;
+
+    uniform mat4 uNormalMatrix;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    varying highp vec2 vTextureCoord;
+    varying highp vec3 vLighting;
+
+    void main(void) {
         gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
         vTextureCoord = aTextureCoord;
-    
+
         // Apply lighting effect
-    
+
         highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
         highp vec3 directionalLightColor = vec3(1, 1, 1);
         highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
-    
+
         highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-    
+
         highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
         vLighting = ambientLight + (directionalLightColor * directional);
-      }
+    }
     `;
     
     //glsl fragment shader
     export const my_glsl_fsSource = `
+    #version 100
     varying highp vec2 vTextureCoord;
     varying highp vec3 vLighting;
     
     uniform sampler2D uSampler;
     
     void main(void) {
-      highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
-    
-      gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
+        highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
+
+        gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
     }
     `;
 //vec3(0.2, 0.2, 0.2); \
     export const my_glsl_fs_str = `
+    #version 100
     precision mediump float; \
     varying vec3 position_eye, normal_eye, out_Kd;	\
     varying mat4 out_Matrix_View; \
