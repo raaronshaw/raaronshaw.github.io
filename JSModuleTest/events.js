4a8127d6aec6649d6db71db39fcba8757f218132
raaronshaw <rashaw@gmail.com>
2025-05-11 05:01:12 -0700

    using quaternions for pitch and heading. Challenges with free camera location not computing intuitively.

--- JSModuleTest/events.js
+++ JSModuleTest/events.js
@@ -8,6 +8,8 @@ let m_xAxis = [1.0, 0.0, 0.0];
 let m_yAxis = [0.0, 1.0, 0.0];
 let m_zAxis = [0.0, 0.0, 1.0];
 let m_viewDir = [0.0, 0.0, -1.0];
+let cam_yaw = 0;
+let test_orientation = quat.create();
 
 function modifyText() {
     const t2 = document.getElementById("t2");
@@ -23,6 +25,8 @@ function modifyText() {
     }
  export function startCanvasEvents(entities)
  {
+  //quat q (angle, x, y, z);
+  //quat(test_orientation= quat(0.0, 0.0, 0.0, 0.0);
   document.getElementById("Layout").addEventListener("click", move, false);
   document.addEventListener("keypress", handleKeyPress, false);
   document.getElementById("Layout").addEventListener("contextmenu", (event) => { event.preventDefault(); /* show a custom context menu*/ });
@@ -50,6 +54,7 @@ function modifyText() {
   {
     mouseRightDown = true;
     mousePos = {x:event.clientX, y:event.clientY};
+    //vewMatrix
   }
   else mouseRightDown = false;
  }
@@ -76,10 +81,25 @@ function mouseMove(event)
     let heading = deltaX;
     let pitch = deltaY;
     if (heading != 0.0) {
-      mat4.rotate(viewMatrix, viewMatrix, degToRad(heading), [0.0, 1.0, 0.0]);
+      let deltaquat = quat.create();
+      quat.setAxisAngle(deltaquat, [0.0,1.0,0.0], degToRad(heading));
+      quat.multiply(test_orientation, test_orientation, deltaquat);
+      let T = mat4.create();
+      let R = mat4.create();
+      mat4.translate(T, mat4.create(), [-m_eye[0], -m_eye[1], -m_eye[2]]);
+      mat4.fromQuat(R, test_orientation);
+      mat4.multiply(viewMatrix, T, R);
+      
     }
     if (pitch != 0.0) {
-      mat4.rotate(viewMatrix, viewMatrix, degToRad(pitch), [1.0, 0.0, 0.0]);
+      let deltaquat = quat.create();
+      quat.setAxisAngle(deltaquat, [1.0,0.0,0.0], degToRad(pitch));
+      quat.multiply(test_orientation, test_orientation, deltaquat);
+      let T = mat4.create();
+      let R = mat4.create();
+      mat4.translate(T, mat4.create(), [-m_eye[0], -m_eye[1], -m_eye[2]]);
+      mat4.fromQuat(R, test_orientation);
+      mat4.multiply(viewMatrix, T, R);
     }
   }  
 }
@@ -95,86 +115,56 @@ function handleMouseDown(event) {
     }
 
     var direction = vec3.create();
-    function handleKeyPress(e) {
-        var keyPressed = e.code;
-        let testdirection = vec4.create();
-        let deltaDirection = 0.1;
-        let deltaMovement = 0;
-        if (keyPressed == 'KeyA')//a
-        {
-            direction = [-10, 0, 0];w
-            
-        }
-        else if (keyPressed == 'KeyD')//d
-        {
-            direction = [10, 0, 0];
-        }
-        else if (keyPressed == 'KeyS')//s
-        {
-            direction = [0, 0, -10];
-        }
-        else if (keyPressed == 'KeyW')//w
-        {
-            let tempmat = mat4.create();
-            m_eye[0]+=viewMatrix[2]*deltaDirection; 
-            m_eye[1]+=viewMatrix[6]*deltaDirection;
-            m_eye[2]+=viewMatrix[10]*deltaDirection;
-            
-            let trans = mat4.create();
-            trans[3] =viewMatrix[2]*deltaDirection; 
-            trans[7] =viewMatrix[6]*deltaDirection;
-            trans[11]=viewMatrix[10]*deltaDirection;
-            
+    function handleKeyPress(e) 
+    {
+      var keyPressed = e.code;
+      let cam_speed = 1;//deltaDirection = 0.1;
+      let cam_moved = 0;
+      if (keyPressed == 'KeyA')//a
+      {
+        m_eye[0] -= cam_speed * 0.1;//elapsed_seconds;
+        cam_moved = true;
+      }
+      else if (keyPressed == 'KeyD')//d
+      {
+        m_eye[0] += cam_speed * 0.1;//elapsed_seconds;
+        cam_moved = true;
+      }
+      else if (keyPressed == 'KeyS')//s
+      {
+        m_eye[2] += cam_speed * 0.1;//elapsed_seconds;
+        cam_moved = true;
+          // direction = [0, 0, -10];
+      }
+      else if (keyPressed == 'KeyW')//w
+      {
+        m_eye[2] -= cam_speed * 0.1;//elapsed_seconds;
+        cam_moved = true;
+      }
+      else if (keyPressed == 'KeyR')//r
+      {
+          // direction = [0, 10, 0];
+      }
+      else if (keyPressed == 'KeyF')//f
+      {
+          //direction = [0, -10, 0];
+      }
+      else if (keyPressed == 'KeyZ')//z
+      {
+          // vec3.copy(z_eye, m_eye);
+          // direction = [0, 0, 0];
+      }
+      // else direction = [0, 0, 0];
+      if (cam_moved) 
+      { 
+        let T = mat4.create();
+        let R = mat4.create();
+        mat4.translate(T, mat4.create(), [-m_eye[0], -m_eye[1], -m_eye[2]]);
+        mat4.fromQuat(R, test_orientation);
+        mat4.multiply(viewMatrix, T, R);
+      }
 
-            viewMatrix[2] =viewMatrix[2]*deltaDirection; 
-            viewMatrix[6] =viewMatrix[6]*deltaDirection;
-            viewMatrix[10]=viewMatrix[10]*deltaDirection;
-mat4.multiply(viewMatrix, viewMatrix, trans);
-            //mat4.invert(tempmat, viewMatrix);
-            //mat4.transpose(viewMatrix, tempmat);
-
-            //testdirection = [viewMatrix[2], viewMatrix[6], viewMatrix[10], viewMatrix[14]];
-            //vec4.normalize(testdirection, testdirection);
-            //testdirection[3] = testdirection[3]+1;
-            //viewMatrix[10]--;//=viewMatrix[0]+1;
-            return;
-        }
-        else if (keyPressed == 'KeyR')//r
-        {
-            direction = [0, 10, 0];
-        }
-        else if (keyPressed == 'KeyF')//f
-        {
-            direction = [0, -10, 0];
-        }
-        else if (keyPressed == 'KeyZ')//z
-        {
-            vec3.copy(z_eye, m_eye);
-            direction = [0, 0, 0];
-        }
-        else direction = [0, 0, 0];
-
-        var forwards = vec3.create();
-        forwards = vec3.cross(forwards, WORLD_YAXIS, m_xAxis);
-        vec3.normalize(forwards, forwards);
-        var eye = vec3.create();
-        vec3.copy(eye, m_eye);
-        vec3.scaleAndAdd(eye, eye, m_xAxis, direction[0]);
-        vec3.scaleAndAdd(eye, eye, WORLD_YAXIS, direction[1]);
-        vec3.scaleAndAdd(eye, eye, forwards, direction[2]);
-        vec3.copy(m_eye, eye);
-
-        //BEGIN UPDATE VIEW MATRIX//
-        mat4.fromQuat(viewMatrix, [m_orientation[1], m_orientation[2], m_orientation[3], m_orientation[0]]);
-        m_xAxis = [viewMatrix[0], viewMatrix[4], viewMatrix[8]];
-        m_yAxis = [viewMatrix[1], viewMatrix[5], viewMatrix[9]];
-        m_zAxis = [viewMatrix[2], viewMatrix[6], viewMatrix[10]];
-        m_viewDir = -m_zAxis;
-        viewMatrix[12] = -vec3.dot(m_xAxis, m_eye);
-        viewMatrix[13] = -vec3.dot(m_yAxis, m_eye);
-        viewMatrix[14] = -vec3.dot(m_zAxis, m_eye);
-        //END UPDATE VIEW MATRIX//
-        return;
+      return;
     }
 
     function handleMouseMove(event) {
